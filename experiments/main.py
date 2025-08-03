94ed63b7f5bf18a78c8e12155db11dc6a2aa0954
Giacomo Fidone <80768580+gfidone@users.noreply.github.com>
2025-08-04 00:24:57 +0200

    Add files via upload

--- experiments/main.py
+++ experiments/main.py
@@ -1,5 +1,5 @@
 import json
-from simulator import Simulator
+from classes.simulator import Simulator
 
 with open('config.json', 'r') as file:
     config = json.load(file)
@@ -8,4 +8,3 @@ cosmos = Simulator(**config.get('init', {}))
 
 if __name__ == '__main__':
     cosmos.run(**config.get('run', {}))
-    cosmos.export(**config.get('export', {}))
