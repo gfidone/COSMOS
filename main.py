9bd374ff5cd2c2af43cce7770d955af5c7c04522
Giacomo Fidone <80768580+gfidone@users.noreply.github.com>
2025-08-04 00:41:19 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+import json
+from classes.simulator import Simulator
+
+with open('config.json', 'r') as file:
+    config = json.load(file)
+
+cosmos = Simulator(**config.get('init', {}))
+
+if __name__ == '__main__':
+    cosmos.run(**config.get('run', {}))
